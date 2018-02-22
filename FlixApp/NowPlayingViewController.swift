ea6e32f9d94afdf6a5577f2231ca7c09f8cbdcd1
Sam <samuel.raymond23@yahoo.com>
2018-02-22 11:43:42 -0500

    Updated the main Storyboard

--- FlixApp/NowPlayingViewController.swift
+++ FlixApp/NowPlayingViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        tableView.dataSource = self
+        
+      let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=dca4026e34b5abb1139f4dfbe652ac3c")!
       let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
       let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
     
@@ -39,7 +44,19 @@ class NowPlayingViewController: UIViewController {
       
     }
     
+        
+        }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-}
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+        
+    }
+    }
+
+
 
